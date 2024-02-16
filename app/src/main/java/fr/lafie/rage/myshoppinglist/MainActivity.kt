97e23beaf3402d4de8328be124b414b0bf634b53
lafie-rage <corentin.destrez62@gmail.com>
2024-02-16 16:00:24 +0100

    Beginning of navigation

--- app/src/main/java/fr/lafie/rage/myshoppinglist/MainActivity.kt
+++ app/src/main/java/fr/lafie/rage/myshoppinglist/MainActivity.kt
@@ -3,38 +3,22 @@ package fr.lafie.rage.myshoppinglist
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.ui.Modifier
-import fr.lafie.rage.myshoppinglist.data.dto.ShoppingItem
-import fr.lafie.rage.myshoppinglist.ui.components.ShoppingItemCard
-import fr.lafie.rage.myshoppinglist.ui.theme.MyShoppingListTheme
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import fr.lafie.rage.myshoppinglist.ui.page.ShoppingListPage
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val shoppingItem = ShoppingItem(
-            id = 0,
-            label = "Pomme de terre",
-            count = 1f,
-            unit = "Kg",
-        )
         setContent {
-            MyShoppingListTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Column(modifier = Modifier.fillMaxSize()) {
-                        ShoppingItemCard(shoppingItem)
-                        ShoppingItemCard(shoppingItem)
-                        ShoppingItemCard(shoppingItem)
-                        ShoppingItemCard(shoppingItem)
-                    }
+            NavHost(
+                navController = rememberNavController(),
+                startDestination = "shopping-list",
+            ) {
+                composable("shopping-list") {
+                    ShoppingListPage()
                 }
             }
         }
